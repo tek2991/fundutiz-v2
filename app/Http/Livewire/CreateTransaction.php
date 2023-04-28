bffff6f6797d4a5fb4e5869b36688fb8b8c6a9db
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2023-04-28 17:50:53 +0530

    Updated

--- app/Http/Livewire/CreateTransaction.php
+++ app/Http/Livewire/CreateTransaction.php
@@ -17,9 +17,13 @@ class CreateTransaction extends Component
     public $funds;
     public $approvers;
 
+    // Transaction type ids
+    public $debitTypeId;
+    public $creditTypeId;
+
     // Dynamic Fund Variables
-    public $currentBalance = 1000;
-    public $balanceAfterTransaction = -500;
+    public $currentBalance;
+    public $balanceAfterTransaction;
 
     // Show debit fields
     public $showDebitFields = true;
@@ -54,23 +58,100 @@ class CreateTransaction extends Component
         $this->funds = $this->currentOffice->funds;
         $this->approvers = Approver::all();
 
+        // Set the transaction type ids
+        $this->debitTypeId = $this->transactionTypes->where('name', 'Debit')->first()->id;
+        $this->creditTypeId = $this->transactionTypes->where('name', 'Credit')->first()->id;
+
         // Set the form data for the current financial year, current user and office
         $this->financialYearId = $this->activeFinancialYear->id;
         $this->officeId = $this->currentOffice->id;
         $this->createdBy = auth()->user()->id;
 
         // Set the default transaction type
-        $this->transactionTypeId = $this->transactionTypes->where('name', 'Debit')->first()->id;
+        $this->transactionTypeId = $this->debitTypeId;
+    }
+
+    public function setCurrentBalance()
+    {
+        $this->currentBalance = $this->funds->find($this->fundId)->getFyBalance();
+    }
+
+    public function calcBalanceAfterTransaction()
+    {
+        if($this->transactionTypeId == $this->debitTypeId)
+        {
+            $this->balanceAfterTransaction = $this->currentBalance - $this->amount;
+        }
+        else
+        {
+            $this->balanceAfterTransaction = $this->currentBalance + $this->amount;
+        }
+    }
+
+    public function updatedFundId($value)
+    {
+        $this->setCurrentBalance();
+        $this->calcBalanceAfterTransaction();
     }
 
     public function updatedTransactionTypeId($value)
     {
-        $this->transactionTypeId = $value;
-        $this->showDebitFields = $this->transactionTypeId == $this->transactionTypes->where('name', 'Debit')->first()->id;
+        $this->showDebitFields = $this->transactionTypeId == $this->debitTypeId;
+        $this->calcBalanceAfterTransaction();
+    }
+
+    public function updatedAmount($value)
+    {
+        $this->calcBalanceAfterTransaction();
+    }
+
+    public function rules(){
+        return [
+            'transactionTypeId' => 'required|exists:transaction_types,id',
+            'financialYearId' => 'required|exists:financial_years,id',
+            'officeId' => 'required|exists:offices,id',
+            'createdBy' => 'required|exists:users,id',
+            'fundId' => 'required|exists:funds,id',
+            'amount' => 'required|numeric|min:1',
+            'fileNumber' => 'required|string',
+            'approved_at' => 'required|date',
+            'approverId' => 'nullable|required_if:transactionTypeId,' . $this->debitTypeId . '|exists:approvers,id',
+            'incurred' => 'nullable|required_if:transactionTypeId,' . $this->debitTypeId . '|boolean',
+            'item' => 'nullable|required_if:transactionTypeId,' . $this->debitTypeId . '|string',
+            'vendorName' => 'nullable|required_if:transactionTypeId,' . $this->debitTypeId . '|string',
+            'gemContractNumber' => 'nullable|required_if:transactionTypeId,' . $this->debitTypeId . '|string',
+            'gemNonAvailabilityCertificateNumber' => 'nullable|required_if:transactionTypeId,' . $this->debitTypeId . '|string',
+            'notGemRemarks' => 'nullable|required_if:transactionTypeId,' . $this->debitTypeId . '|string',
+            'confirmDeficitTransaction' => 'nullable|required_if:balanceAfterTransaction,<,0|boolean',
+            'confirmTransaction' => 'required|boolean',
+        ];
     }
 
     public function submit()
     {
+        $this->validate();
+
+        // Create the transaction
+        $transaction = $this->currentOffice->transactions()->create([
+            'transaction_type_id' => $this->transactionTypeId,
+            'financial_year_id' => $this->financialYearId,
+            'office_id' => $this->officeId,
+            'created_by' => $this->createdBy,
+            'fund_id' => $this->fundId,
+            'amount' => $this->amount,
+            'file_number' => $this->fileNumber,
+            'approved_at' => $this->approved_at,
+            'approver_id' => $this->transactionTypeId == $this->debitTypeId ? $this->approverId : null,
+            'incurred' => $this->transactionTypeId == $this->debitTypeId ? $this->incurred : null,
+            'item' => $this->transactionTypeId == $this->debitTypeId ? $this->item : null,
+            'vendor_name' => $this->transactionTypeId == $this->debitTypeId ? $this->vendorName : null,
+            'gem_contract_number' => $this->transactionTypeId == $this->debitTypeId ? $this->gemContractNumber : null,
+            'gem_non_availability_certificate_number' => $this->transactionTypeId == $this->debitTypeId ? $this->gemNonAvailabilityCertificateNumber : null,
+            'not_gem_remarks' => $this->transactionTypeId == $this->debitTypeId ? $this->notGemRemarks : null,
+        ]);
+
+        // Redirect to the transaction show page
+        return redirect()->route('transactions.show', $transaction)->banner('Transaction recordeed successfully!');
     }
 
     public function render()
--- app/Models/Fund.php
+++ app/Models/Fund.php
@@ -21,4 +21,24 @@ class Fund extends Model
     {
         return $this->hasMany(Transaction::class);
     }
+
+    public function getCreaditTransactions()
+    {
+        $creadit_type_id = TransactionType::where('name', 'Creadit')->first()->id;
+        return $this->transactions()->where('transaction_type_id', $creadit_type_id);
+    }
+
+    public function getDebitTransactions()
+    {
+        $debit_type_id = TransactionType::where('name', 'Debit')->first()->id;
+        return $this->transactions()->where('transaction_type_id', $debit_type_id);
+    }
+
+    public function getFyBalance()
+    {
+        $current_fy_id = FinancialYear::where('is_active', true)->first()->id;
+        $debit_transactions = $this->getDebitTransactions()->where('financial_year_id', $current_fy_id)->sum('amount_in_cents');
+        $creadit_transactions = $this->getCreaditTransactions()->where('financial_year_id', $current_fy_id)->sum('amount_in_cents');
+        return $creadit_transactions - $debit_transactions;
+    }
 }
