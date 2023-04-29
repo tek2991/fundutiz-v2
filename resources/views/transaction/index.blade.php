ebe02e23eda641ac5d72d13472385650d34fc3ad
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2023-04-29 16:58:24 +0530

    Increased max-width

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -15,7 +15,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-screen-2xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
                 @livewire('transaction-table')
             </div>
