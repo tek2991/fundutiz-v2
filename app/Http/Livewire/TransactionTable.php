fb8c303410a84559cee9587a8d93d3af25068c52
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2023-05-19 11:36:56 +0530

    Updated transaction table default sort

--- app/Http/Livewire/TransactionTable.php
+++ app/Http/Livewire/TransactionTable.php
@@ -20,6 +20,9 @@ final class TransactionTable extends PowerGridComponent
     use ActionButton;
     use WithExport;
 
+    public string $sortField = 'approved_at';
+    public string $sortDirection = 'desc';
+
     public string $debitIcon = "
         <svg xmlns=\"http://www.w3.org/2000/svg\" class=\"h-6 w-6 ml-1 text-red-500\" fill=\"none\" viewBox=\"0 0 24 24\" stroke=\"currentColor\">
             <path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M15 12a3 3 0 11-6 0 3 3 0 016 0z\"/>
@@ -82,7 +85,7 @@ final class TransactionTable extends PowerGridComponent
         if (!$userIsAdmin) {
             $query->where('office_id', auth()->user()->office_id);
         }
-        
+
         return     $query->with(['transactionType', 'financialYear', 'office', 'fund', 'approver', 'createdBy']);
     }
 
